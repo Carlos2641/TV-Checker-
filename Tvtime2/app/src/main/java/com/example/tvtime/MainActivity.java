330bb4891df9d73066e6746d36a1ba1e27bb81e2
godofanimes <46845347+godofanimes@users.noreply.github.com>
2019-10-09 11:35:44 +0100

    log in e signup

--- Tvtime2/app/src/main/java/com/example/tvtime/MainActivity.java
+++ Tvtime2/app/src/main/java/com/example/tvtime/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.tvtime;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static void startActivity(Context context) {
+        Intent intent = new Intent(context, MainActivity.class);
+        context.startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void signup(View view) { Signup.startActivity(this); }
 }
--- Tvtime2/app/src/main/java/com/example/tvtime/Signup.java
+++ Tvtime2/app/src/main/java/com/example/tvtime/Signup.java
@@ -2,13 +2,23 @@ package com.example.tvtime;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Signup extends AppCompatActivity {
+
+    public static void startActivity(Context context) {
+        Intent intent = new Intent(context, Signup.class);
+        context.startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
     }
+
+    public void login(View view) { MainActivity.startActivity(this); }
 }
