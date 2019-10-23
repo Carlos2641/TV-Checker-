cf777cd91cbc88a5e5be6e05e229cc92b23e7077
godofanimes <46845347+godofanimes@users.noreply.github.com>
2019-10-23 12:43:34 +0100

    list

--- Tvtime2/app/src/main/java/com/example/tvtime/Definicoes.java
+++ Tvtime2/app/src/main/java/com/example/tvtime/Definicoes.java
@@ -6,6 +6,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.lang.reflect.Array;
 
 public class Definicoes extends AppCompatActivity {
 
@@ -14,11 +19,31 @@ public class Definicoes extends AppCompatActivity {
         context.startActivity(intent);
     }
 
+    ListView defenicoeslist;
+
+    String[] items = {"Convidar Amigos","Ajuda","Idioma","Sobre","Notificções","Segurança","Conta","Sair"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_definicoes);
+
+        defenicoeslist=(ListView) findViewById(R.id.defenicoeslist);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,items);
+
+        defenicoeslist.setAdapter(adapter);
+
+        defenicoeslist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+        });
+
+
     }
+
 
     public void homepage(View view) {
         Homepage.startActivity(this);
